7ccaa31883a45ef8671a5312bf2508a4140ca537
sreesoorajs <sreesoorajs@gmail.com>
2020-06-24 17:53:55 +0530

    Added an interface to handle payment requests

---
+++ BusinessRuleEngineApp/IPaymentRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessRuleEngineApp
+{
+    //interface to handle payment request messages
+    interface IPaymentRequest
+    {
+        /// <summary>
+        /// type of payment request
+        /// </summary>
+        string PaymentType { get; set; }
+
+        /// <summary>
+        /// status of the request(sucesss/failure)
+        /// </summary>
+        string RequestStatus { get; set; }
+
+        /// <summary>
+        /// list of sucess/error messages 
+        /// </summary>
+        List<string> RequestMessages { get; set; }
+    }
+}
