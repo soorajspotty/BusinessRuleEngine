150ca06fbb513bd354fdf118935ead384e9074cb
sreesoorajs <sreesoorajs@gmail.com>
2020-06-24 17:58:30 +0530

    Added a new class PaymentRequest with concrete implementation of IPaymentRequest interface

---
+++ BusinessRuleEngineApp/PaymentRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessRuleEngineApp
+{
+    /// <summary>
+    /// A class with concrete implementation of IPaymentRequest interface
+    /// This can be refered using IPaymentRequest type where ever required
+    /// </summary>
+    public class PaymentRequest : IPaymentRequest
+    {
+        public string PaymentType { get; set; }
+
+        public string RequestStatus { get; set; }
+        public List<string> RequestMessages { get; set; }
+    }
+}
